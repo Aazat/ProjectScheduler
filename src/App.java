e221321ba99b0aae4ba069dd1e8cdcaba0b9fa2f
Aazar <shaikhaazar11@gmail.com>
2023-03-12 14:45:26 +0530

    frequencyScheduler work started

--- src/App.java
+++ src/App.java
@@ -1,10 +1,26 @@
 // import Scheduler.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int a = 0, n = 7;
-        for(;a<7;a++)
-            System.out.print(((a+6)%n) + " ");     
-        // System.out.println(num2);
-    }
+        ArrayList<ArrayList<Integer>> table = new ArrayList<ArrayList<Integer>>(7);
+        
+        for(int i = 0; i < 7; i++){
+            table.add(new ArrayList<Integer>());
+        }
+        // ArrayList<Integer> t = new ArrayList<Integer>();
+                
+        // table.add(t);
+        for(int i= 0; i < 7 ; i++){
+            table.get(i).add(i);
+        }
+        for(ArrayList<Integer> x : table){
+            for(Integer y : x ){
+                System.out.print(y);
+            }
+            System.out.println("");
+        }
+
+    }    
 }
